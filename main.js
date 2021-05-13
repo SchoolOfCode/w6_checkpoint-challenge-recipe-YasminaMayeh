6f4a0ec3f378afecddd10cbe60c71134b6317ee8
Yasminamayeh <yasminmayeh@gmail.com>
2021-05-13 02:44:12 +0100

    new code, styling

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const requestUrl = `https://api.edamam.com/search?q=kale&app_id=dd95e482&app_key
 let foodToSearch = null;
 
 function handleRecipeClick() {
+
+
   fetchRecipe(foodToSearch);
 }
 
@@ -19,61 +21,64 @@ async function fetchRecipe(food) {
   let response = await fetch (`https://api.edamam.com/search?q=${foodToSearch}&app_id=dd95e482&app_key=97617334d58bc72d14c9934ffeb52efa`);
   let data = await response.json();
 
-  //iterate(forloop)through all of the recipes 
+  //iterate(forloop) through all of the recipes 
   let allRecipes = data.hits 
-  for (i=0; i<allRecipes.length; i++){
-    addRecipeToWebpage(data.hits[i])
+  for (i=0; i<
+    
+    3; i++){
+    addRecipeToWebpage(data.hits[i], i) // i = 0, 1, 2
   }
-  
 }
 
-// getting the recipe section from the html
-const recipeSection = document.querySelector("#recipe-section")
+function addRecipeToWebpage(recipe, index){
+  // check if first, second or third recipe is being added
+  let recipeDiv = document.querySelector(".first-recipe")
+  if (index===1) {
+    recipeDiv = document.querySelector(".second-recipe")
+  } else if (index===2){
+    recipeDiv = document.querySelector(".third-recipe")
+  }
 
-function addToList(ingredientName){
+  // recipeDiv = <div id= whatever-recipe>
+  // div get <a>, <img>, <ul>
+
+  // <a href = "http.."> chicken</a>
+  // get the <a> tag. add href attribute. add label.
+  const aTag = recipeDiv.getElementsByTagName("a")[0]
+  let recipeUrl = recipe.recipe.url
+  aTag.setAttribute("href", recipeUrl)
+  aTag.innerHTML = recipe.recipe.label
+
+  // get the <img> tag. add image
+  const imgTag = recipeDiv.getElementsByTagName("img")[0]
+  addImage(imgTag, recipe.recipe.image)
+
+  // get the <ul> tag
+  const ulTag = recipeDiv.getElementsByTagName("ul")[0]
+  // add ingrediats as li's 
+  // clear the results after 
+  ulTag.innerHTML = ""
+  let ingredients = recipe.recipe.ingredients
+  ingredients.forEach((ingredient)=>
+    addToList(ulTag, ingredient.text)
+  )
+}
+
+// ingredient list == <ul> </ul>
+function addToList(ingredientList, ingredientName){
   // create the ingredients list everytie we get a recipe
   // <ul test-align = center ></ul>
-  const ingredientList = document.createElement("ul")
   // ingredientList.setAttribute()
   // <li> ingredientName </li>
   let li = document.createElement("li")
   li.innerHTML = ingredientName
   ingredientList.appendChild(li)
-  recipeSection.appendChild(ingredientList)
 }
 
 
-function addImage(imageUrl){
+function addImage(image, imageUrl){
   // <img src = "http.." width, height />
-  let image = document.createElement("img");
   image.setAttribute("src", imageUrl);
   image.setAttribute("width","200");
   image.setAttribute("height", "200");
-  recipeSection.appendChild(image)
-}
-
-
-function addRecipeToWebpage(recipe){
-
-  // <a href = "http.."> chicken</a>
-  // craete the a tag
-  let recipeLink = document.createElement("a")
-  recipeLink.target = "_blank"
-  recipeLink.innerHTML = recipe.recipe.label
-  let recipeUrl = recipe.recipe.url
-  recipeLink.setAttribute("href", recipeUrl)
-  recipeSection.appendChild(recipeLink)
-  
-//  find the where the ingredients is in the list 
-//  add to the list of ingrdients
-
-  let ingredients = recipe.recipe.ingredients
-  ingredients.forEach((ingredient)=>
-    addToList(ingredient.text)
-  )
-  let recipeImage = recipe.recipe.image
-  addImage(recipeImage);
-  let br = document.createElement("br")
-  recipeSection.appendChild(br)
-
 }
